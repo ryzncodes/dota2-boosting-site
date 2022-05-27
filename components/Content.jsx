5ba7ca7a6a6c3b42b7d9caed945160da404766e4
[Faiz Kasman] <[faizkasman97@gmail.com]>
2022-05-27 16:38:08 +0800

    added parameters to twospacerec component

--- components/Content.jsx
+++ components/Content.jsx
@@ -1,19 +1,23 @@
 import React from 'react'
 import TwoSpacedRec from './TwoSpacedRec'
 import OneSpacedRec from './OneSpacedRec'
+import D2Logo from '../public/dt2logo.png'
+
+const Platform = "Platform"
+const dota2 = "Dota 2"
 
 function Content() {
   return (
     <div className="h-[502px] bg-[#8A2424]">
         
-        <p className="space-y-[2.5px]">    
+        <div className="space-y-[2.5px]">    
             <h1 className="pt-[25px] text-center text-[15px] font-semibold text-white">
                 DOTA 2 Boosting Service
             </h1>
             <h2 className="text-center text-[12px] font-normal text-white opacity-50">
                 Boosting in every servers.
             </h2>
-        </p>
+        </div>
 
         <div className="h-[390px] w-[430px] border border-[#D47E7E] bg-[#802525] mx-auto mt-[12px] rounded-[15px]">
           
@@ -21,8 +25,8 @@ function Content() {
             Calculate the price of your boost.
           </h1>
 
-          <div>
-          <TwoSpacedRec />
+          <div className="flex">
+          <TwoSpacedRec heading={Platform} value={dota2} logo={D2Logo} />
 
           </div>
         </div>
--- components/TwoSpacedRec.jsx
+++ components/TwoSpacedRec.jsx
@@ -1,18 +1,22 @@
 import React from 'react'
 import Image from 'next/image'
-import D2Logo from '../public/dt2logo.png'
 
-function TwoSpacedRec() {
+function TwoSpacedRec({heading, value, logo}) {
   return (
-    <div className="bg-[#8A2424] w-[120px] h-[28px] ml-[20px] border border-[#DF6363] rounded-[20px]">
-        <div className="flex h-[100%] w-[32px] border border-r-red-700 content-center pt-[9px] pl-[11px]">
+    <div className="flex bg-[#8A2424] w-[120px] h-[28px] ml-[20px] border border-[#DF6363] rounded-[20px] mt-[50px]">
+        <div className="flex h-[100%] w-[32px] border-r border-r-[#DF6363] content-center pt-[9px] pl-[11px]">
             <Image
-                src={D2Logo}
+                src={logo}
                 alt="Picture of Dota 2 Logo"
                 width="10px"
                 height="10px"
-                layout="fixed"            
+                layout="fixed"
             />
+        </div>
+
+        <div className="pl-1 -space-y-[5px]">
+          <h1 className="font-light text-[7px] opacity-70 text-gray-100">{heading}</h1>
+          <h2 className="text-gray-100 italic text-[13px]">{value}</h2>
         </div>
     </div>
   )
